fdfbc4ebc5b7f1ef40affc9de75bb667323f77ff
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-02-04 14:36:36 +0100

    Number Theory Pb

--- D. Kevin and Numbers/Solution.java
+++ D. Kevin and Numbers/Solution.java
@@ -1,6 +1,6 @@
 import java.util.* ; 
 
-public class D {
+public class Solution {
     public static void main( String[] args ){
         Scanner sc = new Scanner( System.in ) ; 
         int t = sc.nextInt() ; 
