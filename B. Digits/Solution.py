8507646ff34f5b806094113eba01f53b6e98a882
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-03-12 22:30:49 +0000

    Number Theory Pb

---
+++ B. Digits/Solution.py
@@ -0,0 +1,16 @@
+def fact(x):
+    if x == 0:
+        return 1
+    return x * fact(x - 1)
+ 
+t = int(input())
+for i in range(t):
+    n, k = map(int, input().split())
+    n = min(n, 6)
+    s = int(str(k) * fact(n))
+    for i in range(1, 10, 2):
+        if s % i == 0:
+            print(i, end =  ' ')
+    print()
+    
+
