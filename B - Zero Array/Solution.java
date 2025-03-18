6a921abe06fadefdc466e10990fbd3827541a06c
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-03-18 17:37:16 +0000

    No sorting needed

--- B - Zero Array/Solution.java
+++ B - Zero Array/Solution.java
@@ -8,7 +8,6 @@ public class Solution {
         for( int i=0 ; i<n ; ++i ){
             arr[i] = sc.nextLong() ; 
         }
-        Arrays.sort(arr) ; 
         long sum = 0L ; 
         long max = 0L ;
         long e = 0L ; 
