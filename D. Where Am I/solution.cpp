5ac79456c295a9913bb10ced2f7299858d4c8219
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-04-03 12:26:50 +0000

    April Fools Pb D

---
+++ D. Where Am I/solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1 ;  
+    // int t ; cin >> t ; 
+    while( t-- ){
+         cout << "36.104300 -115.172835";
+    }
+    return 0;
+}
