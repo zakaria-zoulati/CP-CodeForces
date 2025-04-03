ec27381b8a3000132f5752cd5cd2a18a0d307f4c
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-04-03 11:11:08 +0000

    April Fools 2025 Pb A

---
+++ A. Piecing It Together/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1 ;  
+    while( t-- ){
+        cout << "puzzling" << endl ; 
+    }
+    return 0;
+}
