f7eb72ab28b594bec9b95044ad3d3af58e96302a
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-09-10 00:26:41 +0100

    Contest Pb

---
+++ B. Another Divisibility Problem/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long ; 
+
+void solve() {
+    ll x ; cin >> x ; 
+    ll rs = x << 1 ; 
+    cout << rs << '\n' ; 
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t ; cin >> t ;
+    while( t-- ){
+        solve() ; 
+    }
+}
